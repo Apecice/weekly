25aa4e5608a480b01c377817c69d01af88246246
bmiddle2.backend.hongzhihao <1693969443@qq.com>
2024-06-13 18:03:03 +0800

    添加配置参数

--- src/config.ts
+++ src/config.ts
@@ -4,10 +4,11 @@ export const SITE = {
 	description: '记录工程师 Apec 的不枯燥生活，每周发布，欢迎关注',
 	keywords: 'Apec,Weekly,Blog,后端',
 	icon: 'https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png',
-	pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
+	pic: 'https://raw.githubusercontent.com/Apecice/imagesRepo/main/img/rose.png',
 	homePage: '',
 	blogPage: '',
 	twitterId: '',
 	githubId: 'Apecice',
 	repo: 'apec/weekly',
+	chatUrl: 'https://chat.memotrace.cn/',
 };
